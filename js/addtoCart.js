75c44cd0a58f081897948fc34ef397b89449911f
HabibaShata <habiba.helmy.shata@gmail.com>
2024-01-17 22:45:44 +0200

    creat add to cart functionality  & checkout design

---
+++ js/addtoCart.js
@@ -0,0 +1,216 @@
+import { GetProducts, products } from "./custom.js";
+
+let listProductHtml = document.getElementById("products-Landing");
+let listCartHTML = document.querySelector('.cart-body');
+let iconCart = document.querySelector('.cartLogo');
+let iconCartSpan = document.querySelector('.cartLogo #cntOrders');
+let closeCart = document.querySelector('.cart-clear');
+let checkOut = document.querySelector(".checkout");
+let arrowBack = document.querySelector(".arrowBack");
+let cart = document.querySelector(".cart");
+let lyercartOverlay = document.querySelector(".cart-overlay");
+let temmraryDiv = document.querySelector(".addedSuccess");
+
+var arrCart = [];
+
+//cart events
+iconCart.addEventListener("click", showCart);
+arrowBack.addEventListener("click", hideCart);
+closeCart.addEventListener("click", clearCart);
+
+function showCart() {
+    cart.classList.add("show");
+    lyercartOverlay.classList.add("show");
+}
+
+function hideCart() {
+    lyercartOverlay.classList.remove("show");
+    cart.classList.remove("show");
+}
+function clearCart(e) {
+    arrCart = [];
+    addCartToMemory();
+    addCartToHTML();
+
+  
+  };
+  
+// to make sure Dom[html code] loded 
+var product_Id;
+window.addEventListener("load", function () {
+    var addCartLink = document.querySelectorAll(".addCart");
+    for (var i = 0; i < addCartLink.length; i++) {
+        addCartLink[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            product_Id = parseInt(event.target.parentElement.parentElement.parentElement.parentElement.classList[3].split('=')[1]);      
+            addToCart(product_Id);
+        
+        })
+    }
+
+})
+
+//**   add to cart    / */
+
+var cnt=0;
+const addToCart = (product_id) => {
+
+    //findindex fun return index of ele if it extist in arr else if rturn -1;
+    let positionThisProductInCart = arrCart.findIndex((value) => value.product_id == product_id);
+    console.log(positionThisProductInCart);
+    if (arrCart.length <= 0) {
+        arrCart = [{
+            product_id: product_id,
+            quantity: 1,
+        }];
+        temmraryDiv.style.display = "block";
+        setTimeout(function () {
+            temmraryDiv.style.display = "none";
+        }, 2000)
+        cnt++;
+    } else if (positionThisProductInCart < 0) {
+        arrCart.push({
+            product_id: product_id,
+            quantity: 1
+        });
+        temmraryDiv.style.display = "block";
+        setTimeout(function () {
+            temmraryDiv.style.display = "none";
+        }, 2000)
+        cnt++;
+    } else {
+        alert("Item is already in cart");
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+
+const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(arrCart));
+}
+const addCartToHTML = () => {
+    let totalPrice = document.querySelector('.cart-total');
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    let total = 0;
+
+    if (arrCart.length > 0) {
+        arrCart.forEach(item => {
+
+            //     console.log(item);
+            totalQuantity = totalQuantity + item.quantity;
+            total=item.quantity*products[item.product_id - 1].price;
+            let newItem = document.createElement('div');
+            newItem.classList.add('item');
+          //  console.log(products);
+            let info = products[product_Id];
+            //    console.log(info['images'][0]);
+            listCartHTML.appendChild(newItem);
+            newItem.innerHTML =
+                `
+                <div class="cart-item" data-id="${item.product_id}">
+                <img src="${products[item.product_id - 1].images[0]}" />
+                <div class="cart-item-detail">
+                  <h3>${products[item.product_id - 1].productName}</h3>
+                  <h5>${products[item.product_id - 1].price}</h5>
+                  <div class="cart-item-amount">
+                  <i class="fa-solid fa-minus bi "data-btn="decr"></i>
+                  <span class="qty">${item.quantity}</span>
+                  <i class="fa-solid fa-plus bi"data-btn="incr"></i>
+      
+                    <span class="cart-item-price">
+                      ${products[item.product_id - 1].price * item.quantity}
+                    </span>
+                    <i class="fa-solid fa-trash-can deleteItem"></i>
+                  </div>
+                </div>
+              </div>
+              `;
+
+            //cnt orders
+            iconCartSpan.innerText = cnt;
+            console.log(products[item.product_id - 1].price * item.quantity);
+
+        })
+        //delete clicked item 
+        let itemsFromCart = document.querySelectorAll(".cart-item-detail");
+        for (var i = 0; i < itemsFromCart.length; i++) {
+            itemsFromCart[i].addEventListener("click", function (e) {
+                if (e.target.classList.contains("deleteItem")) {
+                    var itemDeleted = parseInt(e.target.parentElement.parentElement.parentElement.dataset.id);
+                      updateCart(itemDeleted);
+                }
+            })
+        }
+        //console.log(itemsFromCart);
+
+    }else{
+
+    let items=document.querySelectorAll(".item");
+     cnt=0
+      iconCartSpan.innerText=cnt;
+      listCartHTML.removeChild(items);
+     // console.log(arrCart.length);
+    }
+}
+
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    //console.log(event.target.dataset.btn);
+    if (positionClick.dataset.btn == "decr" || positionClick.dataset.btn == "incr") {
+        let product_id = parseInt(positionClick.parentElement.parentElement.parentElement.dataset.id);
+        console.log(product_Id);
+        let type = 'decr';
+        if (event.target.dataset.btn == "incr") {
+            type = 'incr';
+        }
+        changeQuantityCart(product_id, type);
+    }
+})
+const changeQuantityCart = (product_id, type) => {
+    let positionItemInCart = arrCart.findIndex((value) => value.product_id == product_id);
+    // console.log(positionItemInCart);
+    if (positionItemInCart >= 0) {
+      //  let info = arrCart[positionItemInCart];
+        switch (type) {
+            case 'incr':
+                arrCart[positionItemInCart].quantity = arrCart[positionItemInCart].quantity + 1;
+                break;
+
+            default:
+                let changeQuantity = arrCart[positionItemInCart].quantity - 1;
+                if (changeQuantity >= 1) {
+                    arrCart[positionItemInCart].quantity = changeQuantity;
+                } 
+                break;
+        }
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+
+// fun delete&update 
+const updateCart = (itemDeleted) => {
+    var containerDeletedItem = document.querySelectorAll(".cart-item");
+    // console.log(containerDeletedItem);
+    // let positionItemInCart = arrCart.findIndex((value) => value.product_id == itemDeleted);
+    // console.log("index arrtCart", positionItemInCart);
+    // console.log("id item deleted", itemDeleted);//index 
+    var conf = confirm(`Do you really want to remove  ${products[itemDeleted - 1].productName} from cart? `);
+   
+    if (conf) {
+        //delete from arrCart
+        arrCart = arrCart.filter((x) => x.product_id !== itemDeleted);
+        //delete from html
+        for (var i = 0; i < containerDeletedItem.length; i++) {
+            if (containerDeletedItem[i].dataset.id == itemDeleted) {
+                containerDeletedItem[i].remove();
+            }
+        }
+         addCartToMemory();
+         cnt--;
+         iconCartSpan.innerText = cnt;
+      //  console.log(arrCart);
+    }
+}
+
--- js/custom.js
+++ js/custom.js
@@ -8,44 +8,42 @@
 // getYear();
 
 //Product class function
-class Product
-{
-    constructor(_productId, _productName, _category, _sellerName, _images, _price, _description, _options)
-    {
-        this.productId = _productId;
-        this.productName = _productName;
-        this.category = _category;
-        this.sellerName = _sellerName;
-        this.images = _images;
-        this.price = _price;
-        this.description = _description;
-        this.options = _options;
-    }
+class Product {
+   constructor(_productId, _productName, _category, _sellerName, _images, _price, _description, _options) {
+      this.productId = _productId;
+      this.productName = _productName;
+      this.category = _category;
+      this.sellerName = _sellerName;
+      this.images = _images;
+      this.price = _price;
+      this.description = _description;
+      this.options = _options;
+   }
 }
 
 //random data
 let products = [
-      new Product(1, "Jewellery", "Jewellery", "Seller1", ["images/p1.png", "images/p1.png", "images/p1.png"], 50000, "This is a Jewellery", ["Black", "White", "Red"]),
-      new Product(2, "Accessories", "Accessories", "Seller2", ["images/p2.png", "images/p2.png", "images/p2.png"], 20000, "This is a Accessories", ["Black", "White", "Red"]),
-      new Product(3, "Artwork", "Artwork", "Seller3", ["images/p3.png", "images/p3.png", "images/p3.png"], 30000, "This is a Artwork", ["Black", "White", "Red"]),
-      new Product(4, "Accessories", "Accessories", "Seller4", ["images/p4.png", "images/p4.png", "images/p4.png"], 1000, "This is a Accessories", ["Black", "White", "Red"]),
-      new Product(5, "Artwork", "Artwork", "Seller5", ["images/p5.png", "images/p5.png", "images/p5.png"], 2000, "This is Artwork", ["Black", "White", "Red"]),
-      new Product(6, "Pet-supplies", "Pet-supplies", "Seller6", ["images/p6.png", "images/p6.png", "images/p6.png"], 3000, "This is Pet-supplies", ["Black", "White", "Red"]),
-      new Product(7, "Sweets", "Sweets", "Seller7", ["images/p7.png", "images/p7.png", "images/p7.png"], 4000, "This is Sweets", ["Black", "White", "Red"]),
-      new Product(8, "Sweets", "Sweets", "Seller8", ["images/p8.png", "images/p8.png", "images/p8.png"], 5000, "This is Sweets", ["Black", "White", "Red"]),
-      new Product(9, "Jewellery", "Jewellery", "Seller9", ["images/p9.png", "images/p9.png", "images/p9.png"], 6000, "This is Jewellery", ["Black", "White", "Red"]),
-      new Product(10, "Accessories", "Accessories", "Seller10", ["images/p10.png", "img/Accessories.jpg", "img/hat.jpg"], 7000, "This is hat", ["Black", "White", "Red"]),
-      new Product(11, "Artwork", "Accessories", "Seller11", ["images/p11.png", "img/Accessories.jpg", "img/hat.jpg"], 8000, "This is hat", ["Black", "White", "Red"]),
-      new Product(12, "Accessories", "Accessories", "Seller12", ["images/p12.png", "img/Accessories.jpg", "img/hat.jpg"], 9000, "This is hat", ["Black", "White", "Red"]),
-      new Product(13, "Artwork", "Accessories", "Seller13", ["images/p1.png", "img/Accessories.jpg", "img/hat.jpg"], 10000, "This is hat", ["Black", "White", "Red"]),
-      new Product(14, "Accessories", "Accessories", "Seller14", ["images/p2.png", "img/Accessories.jpg", "img/hat.jpg"], 11000, "This is hat", ["Black", "White", "Red"]),
-      new Product(15, "Artwork", "Accessories", "Seller15", ["images/p3.png", "img/Accessories.jpg", "img/hat.jpg"], 12000, "This is hat", ["Black", "White", "Red"]),
-      new Product(16, "Accessories", "Accessories", "Seller16", ["images/p4.png", "img/Accessories.jpg", "img/hat.jpg"], 13000, "This is hat", ["Black", "White", "Red"]),
-      new Product(17, "Artwork", "Accessories", "Seller17", ["images/p5.png", "img/Accessories.jpg", "img/hat.jpg"], 14000, "This is hat", ["Black", "White", "Red"]),
-      new Product(18, "Accessories", "Accessories", "Seller18", ["images/p6.png", "img/Accessories.jpg", "img/hat.jpg"], 15000, "This is hat", ["Black", "White", "Red"]),
-      new Product(19, "Artwork", "Accessories", "Seller19", ["images/p7.png", "img/Accessories.jpg", "img/hat.jpg"], 16000, "This is hat", ["Black", "White", "Red"]),
-      new Product(20, "Accessories", "Accessories", "Seller20", ["images/p8.png", "img/Accessories.jpg", "img/hat.jpg"], 17000, "This is hat", ["Black", "White", "Red"]),
-      new Product(21, "Artwork", "Accessories", "Seller21", ["images/p9.png", "img/Accessories.jpg", "img/hat.jpg"], 18000, "This is hat", ["Black", "White", "Red"]),
+   new Product(1, "Jewellery", "Jewellery", "Seller1", ["images/p1.png", "images/p1.png", "images/p1.png"], 50000, "This is a Jewellery", ["Black", "White", "Red"]),
+   new Product(2, "Accessories", "Accessories", "Seller2", ["images/p2.png", "images/p2.png", "images/p2.png"], 20000, "This is a Accessories", ["Black", "White", "Red"]),
+   new Product(3, "Artwork", "Artwork", "Seller3", ["images/p3.png", "images/p3.png", "images/p3.png"], 30000, "This is a Artwork", ["Black", "White", "Red"]),
+   new Product(4, "Accessories", "Accessories", "Seller4", ["images/p4.png", "images/p4.png", "images/p4.png"], 1000, "This is a Accessories", ["Black", "White", "Red"]),
+   new Product(5, "Artwork", "Artwork", "Seller5", ["images/p5.png", "images/p5.png", "images/p5.png"], 2000, "This is Artwork", ["Black", "White", "Red"]),
+   new Product(6, "Pet-supplies", "Pet-supplies", "Seller6", ["images/p6.png", "images/p6.png", "images/p6.png"], 3000, "This is Pet-supplies", ["Black", "White", "Red"]),
+   new Product(7, "Sweets", "Sweets", "Seller7", ["images/p7.png", "images/p7.png", "images/p7.png"], 4000, "This is Sweets", ["Black", "White", "Red"]),
+   new Product(8, "Sweets", "Sweets", "Seller8", ["images/p8.png", "images/p8.png", "images/p8.png"], 5000, "This is Sweets", ["Black", "White", "Red"]),
+   new Product(9, "Jewellery", "Jewellery", "Seller9", ["images/p9.png", "images/p9.png", "images/p9.png"], 6000, "This is Jewellery", ["Black", "White", "Red"]),
+   new Product(10, "Accessories", "Accessories", "Seller10", ["images/p10.png", "img/Accessories.jpg", "img/hat.jpg"], 7000, "This is hat", ["Black", "White", "Red"]),
+   new Product(11, "Artwork", "Accessories", "Seller11", ["images/p11.png", "img/Accessories.jpg", "img/hat.jpg"], 8000, "This is hat", ["Black", "White", "Red"]),
+   new Product(12, "Accessories", "Accessories", "Seller12", ["images/p12.png", "img/Accessories.jpg", "img/hat.jpg"], 9000, "This is hat", ["Black", "White", "Red"]),
+   new Product(13, "Artwork", "Accessories", "Seller13", ["images/p1.png", "img/Accessories.jpg", "img/hat.jpg"], 10000, "This is hat", ["Black", "White", "Red"]),
+   new Product(14, "Accessories", "Accessories", "Seller14", ["images/p2.png", "img/Accessories.jpg", "img/hat.jpg"], 11000, "This is hat", ["Black", "White", "Red"]),
+   new Product(15, "Artwork", "Accessories", "Seller15", ["images/p3.png", "img/Accessories.jpg", "img/hat.jpg"], 12000, "This is hat", ["Black", "White", "Red"]),
+   new Product(16, "Accessories", "Accessories", "Seller16", ["images/p4.png", "img/Accessories.jpg", "img/hat.jpg"], 13000, "This is hat", ["Black", "White", "Red"]),
+   new Product(17, "Artwork", "Accessories", "Seller17", ["images/p5.png", "img/Accessories.jpg", "img/hat.jpg"], 14000, "This is hat", ["Black", "White", "Red"]),
+   new Product(18, "Accessories", "Accessories", "Seller18", ["images/p6.png", "img/Accessories.jpg", "img/hat.jpg"], 15000, "This is hat", ["Black", "White", "Red"]),
+   new Product(19, "Artwork", "Accessories", "Seller19", ["images/p7.png", "img/Accessories.jpg", "img/hat.jpg"], 16000, "This is hat", ["Black", "White", "Red"]),
+   new Product(20, "Accessories", "Accessories", "Seller20", ["images/p8.png", "img/Accessories.jpg", "img/hat.jpg"], 17000, "This is hat", ["Black", "White", "Red"]),
+   new Product(21, "Artwork", "Accessories", "Seller21", ["images/p9.png", "img/Accessories.jpg", "img/hat.jpg"], 18000, "This is hat", ["Black", "White", "Red"]),
 ]
 
 //add the products to the local storage
@@ -53,64 +51,41 @@ localStorage.setItem("products", JSON.stringify(products));
 
 
 // client section owl carousel
-$(function(){
-   //  $(".client_owl-carousel").owlCarousel({
-   //      loop: true,
-   //      margin: 0,
-   //      dots: false,
-   //      nav: true,
-   //      navText: [],
-   //      autoplay: true,
-   //      autoplayHoverPause: true,
-   //      navText: [
-   //          '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-   //          '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-   //      ],
-   //      responsive: {
-   //          0: {
-   //              items: 1
-   //          },
-   //          768: {
-   //              items: 2
-   //          },
-   //          1000: {
-   //              items: 2
-   //          }
-   //      }
-   //  });
+$(function () {
+
    let productCards = GetProducts(9);
    document.getElementById("products-Landing").innerHTML = productCards;
 })
 
-function GetProducts(maxNumber, productsList)
-{
+function GetProducts(maxNumber, productsList) {
    let products;
-   if (productsList!=undefined) {
+   if (productsList != undefined) {
       products = productsList;
-   } 
+   }
    else {
       products = JSON.parse(localStorage.getItem("products"));
    }
 
-   if(maxNumber == -1)
-   {
+   if (maxNumber == -1) {
       maxNumber = products.length;
    }
    let productCards = "";
-   for(let i = 0; i < maxNumber; i++)
-   {
-       productCards += `<div class="col-sm-6 col-md-4 col-lg-4">
-       <div class="box">
-           <div class="option_container">
+   for (let i = 0; i < maxNumber; i++) {
+      productCards +=
+      
+         `<div class="col-sm-6 col-md-4 col-lg-4 data-id=${products[i].productId}">
+
+           <div class="box">
+              <div class="option_container">
                    <div class="options">
-                      <a href="" class="option1">
+                      <a href="" class="option1 addCart">
                       Add to cart
                       </a>
                       <a href="productDetails.html?productId=${products[i].productId}" class="option2">
                       View Details
                       </a>
                    </div>
-                </div>
+               </div>
                 <div class="img-box">
                    <img src="${products[i].images[0]}" alt="">
                 </div>
@@ -123,8 +98,12 @@ function GetProducts(maxNumber, productsList)
                    </h6>
                 </div>
              </div>
+         
           </div>`;
+
    }
+
+
 
    return productCards;
 }
@@ -132,11 +111,11 @@ function GetProducts(maxNumber, productsList)
 
 /** google_map js **/
 function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+   var mapProp = {
+      center: new google.maps.LatLng(40.712775, -74.005973),
+      zoom: 18,
+   };
+   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
-export {GetProducts, products};
+export { GetProducts, products };
