a6f3ee1caec6af0a660115cb4a80f8465aab7ceb
Omar <omarelemam49141@gmail.com>
2024-01-17 07:09:33 +0200

    modifying the authentication and adding the user CRUD v1.0

--- js/products.js
+++ js/products.js
@@ -3,16 +3,63 @@ import { GetProducts, products } from "./custom.js";
 let categories = ["All", "Jewellery", "Accessories", "Artwork", "Pet-supplies", "Sweets"];
 let filter = "All";
 
+function searchProductsByName(productName)
+{
+    //filter the array according to the name
+    
+    let searchedArr = products.filter(function(product){
+        if (filter != "All")
+        {
+            return product.productName.toLowerCase().includes(productName.toLowerCase()) && product.category==filter;
+        }
+        else 
+        {
+            return product.productName.toLowerCase().includes(productName.toLowerCase());
+        }
+    });
+    //update the products display with the searched array
+    document.getElementById("all-products-section").innerHTML = GetProducts(searchedArr.length, searchedArr);
+}
+
 window.addEventListener("load", function(){
-    let allProducts = GetProducts(-1);
-    this.document.getElementById("all-products-section").innerHTML = allProducts;
+    //Check if the user is logged in then change the Account dropdown menu and change the links urls
+    if (this.localStorage.getItem("loggedInUser")!=null) {
+        document.getElementById("loggedInUser").innerHTML = "Welcome, " +  localStorage.getItem("loggedInUser").substring(0, localStorage.getItem("loggedInUser").indexOf("@"));
+        //add the list items to the dropdown menu
+        let userDropDown = document.getElementById("user-DropDown");
+        userDropDown.innerHTML = `
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Orders</a></li>
+        <li><a href="#" id="logOut">Log out</a></li>
+        `;
+        //change the a href to the customer page
+        document.getElementById("homeLink").setAttribute("href", "customer.html");
+    } else {
+        loggedInUser.innerHTML = "Account";
+        //add the list items to the dropdown menu
+        let userDropDown = document.getElementById("user-DropDown");
+        userDropDown.innerHTML = `
+        <li><a href="sign-up/sign-up.html">Sign up</a></li>
+        <li><a href="Login/login.html">Log in</a></li>
+        `;
+        //change the a href to the index page
+        document.getElementById("homeLink").setAttribute("href", "index.html");
+    }
+    //categories
     MakingCategories();
+
+    //logout
+    LogOut();
+
+    let allProducts = GetProducts(-1);
+    document.getElementById("all-products-section").innerHTML = allProducts;
 
     //filtering
     let categoryItems = document.getElementById("categories").children;
     document.getElementById("categories").addEventListener("click", function(e){
         if(e.target.nodeName=="LI")
         {
+            filter = e.target.innerHTML;
             //Removing the active class from all list items
             for (let i = 0; i < categoryItems.length; i++) {
                 categoryItems[i].classList.remove("active");
@@ -36,7 +83,12 @@ window.addEventListener("load", function(){
             //Updating the displayed products with the filtered products
             document.getElementById("all-products-section").innerHTML = allFilteredProducts;
         }
-    }) 
+    })
+
+    //adding search functionality
+    document.getElementById("search-input").addEventListener("keyup", function(){
+        searchProductsByName(this.value);
+    })
 })
 
 function MakingCategories()
@@ -50,4 +102,14 @@ function MakingCategories()
         }
         document.getElementById("categories").innerHTML += `<li class="list-inline-item py-4 px-4">${categories[i]}</li>`;
     }
+}
+
+export function LogOut (){
+    document.getElementById("user-DropDown").addEventListener("click", function(e){
+        if(e.target.id=="logOut")
+        {
+            localStorage.removeItem("loggedInUser");
+            window.location.href = "index.html";
+        }
+    })
 }
