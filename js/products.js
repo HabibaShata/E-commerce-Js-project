6fb31621c29dd5c69fea31b0be9c6175ad268125
Omar <omarelemam49141@gmail.com>
2024-01-19 15:36:05 +0200

    Modifying the navbar appearance from the product section for the admins

--- js/products.js
+++ js/products.js
@@ -26,17 +26,56 @@ function searchProductsByName(productName) {
 
 window.addEventListener("load", function () {
     //Check if the user is logged in then change the Account dropdown menu and change the links urls
-    if (this.localStorage.getItem("loggedInUser") != null) {
-        document.getElementById("loggedInUser").innerHTML = "Welcome, " + JSON.parse(localStorage.getItem("loggedInUser")).userName;
-        //add the list items to the dropdown menu
-        let userDropDown = document.getElementById("user-DropDown");
-        userDropDown.innerHTML = `
-        <li><a href="profile.html">Profile</a></li>
-        <li><a href="#">Orders</a></li>
-        <li><a href="#" id="logOut">Log out</a></li>
-        `;
-        //change the a href to the customer page
-        document.getElementById("homeLink").setAttribute("href", "customer.html");
+    let loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser != null) {
+        if(JSON.parse(loggedInUser).userRole=="admin")
+        {
+            document.querySelector(".content").innerHTML = `
+            
+               <nav class="navbar navbar-expand-lg custom_nav-container ">
+                  <a class="navbar-brand" href="index.html"><img width="250" src="images/logo.png" alt="#" /></a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class=""> </span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item active">
+                           <a class="nav-link" href="customer.html">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label" id="welcome-user">${JSON.parse(loggedInUser).userName} <span class="caret"></span></a>
+                           <ul class="dropdown-menu" id="user-DropDown">
+                              <li><a href="#">Profile</a></li>
+                              <li><a href="usersCRUD.html">Users</a></li>
+                              <li><a href="#" id="logOut">Log out</a></li>
+                           </ul>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="product.html">Products</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="blog_list.html">Orders</a>
+                        </li>     
+                     </ul>
+                  </div>
+               </nav>
+            `;
+        }
+        else 
+        {
+            //add the list items to the dropdown menu
+            let userDropDown = document.getElementById("user-DropDown");
+            userDropDown.innerHTML = `
+            <li><a href="profile.html">Profile</a></li>
+            <li><a href="#">Orders</a></li>
+            <li><a href="#" id="logOut">Log out</a></li>
+            `;
+            //change the a href to the customer page
+            document.getElementById("homeLink").setAttribute("href", "customer.html");
+            document.getElementById("loggedInUser").innerHTML = "Welcome, " + JSON.parse(localStorage.getItem("loggedInUser")).userName;
+        }
+
+        
     } else {
         loggedInUser.innerHTML = "Account";
         //add the list items to the dropdown menu
@@ -55,12 +94,10 @@ window.addEventListener("load", function () {
     LogOut();
 
     let allProducts = GetProducts(-1);
-    console.log(allProducts);
     document.getElementById("all-products-section").innerHTML = allProducts;
 
     //filtering
     let categoryItems = document.getElementById("categories").children;
-    console.log(categoryItems);
     document.getElementById("categories").addEventListener("click", function (e) {
         console.log(e.target);
         console.log(this);
