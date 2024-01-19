3ad151a71e33e88df12740ec75476211175d15b1
Omar <omarelemam49141@gmail.com>
2024-01-19 14:28:33 +0200

    After habiba add to cart and nada product details

--- js/orders.js
+++ js/orders.js
@@ -34,6 +34,9 @@ let orders = [
     new Order(2, "Alice Smith", "456 Oak St, Townsville", "Zara Store", "2022-04-19", 12, 18.0, "Processing", [
         new Item(2, "Accessories", "images/p2.png", "Black", 2, 3.0, 6.0)
     ], "In Transit"),
+    new Order(2, "John Doe", "456 Oak St, Townsville", "Zara Store", "2022-04-19", 12, 18.0, "Processing", [
+        new Item(2, "Accessories", "images/p2.png", "Black", 2, 3.0, 6.0)
+    ], "In Transit"),
     new Order(3, "Bob Johnson", "789 Pine St, Villagetown", "LC WAIKIKI Store", "2022-04-18", 15, 40.0, "Order Shipped", [
         new Item(3, "Artwork", "images/p3.png", "White", 4, 6.25, 25.0)
     ], "Delivered"),
@@ -54,17 +57,19 @@ function GetOrders(ordersList) {
         orders = ordersList;
     else 
         orders = JSON.parse(localStorage.getItem("orders"));
+
+    
     
 
     orders.forEach(order => {
         createdtr = document.createElement("tr");//<tr>
         createdtr.innerHTML = `                         
-          <td>${order.id}</td>
-          <td>${order.date}</td>
-         <td>${order.seller}</td>
-         <td>${order.totalPrice}</td>
-         <td>${order.updateStatus}</td>
-         <td> <a href="orderDetails.html?orderId=${order.id}">View Details</a></td>`
+        <td>${order.id}</td>
+        <td>${order.date}</td>
+        <td>${order.seller}</td>
+        <td>${order.totalPrice}</td>
+        <td>${order.updateStatus}</td>
+        <td> <a href="orderDetails.html?orderId=${order.id}">View Details</a></td>`
         $("tbody")[0].appendChild(createdtr);
     });
 
